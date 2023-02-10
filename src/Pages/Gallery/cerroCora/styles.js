0dd11b8671169878cd2f8b13fa03724584786149
Evymendes <evelyn.laurindo@gmail.com>
2023-02-10 12:21:25 -0300

    Hotfix Inserted the photos

--- src/Pages/Gallery/cerroCora/styles.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const BackText = styled.p`
-  padding-bottom: .75rem;
-  font-size: 1rem;
-  color: var(--color_lightGray);
-  font-family: FiraSans;
-  line-height: 1.8;
-`;
--- src/Pages/Gallery/prazeres/index.js
+++ src/Pages/Gallery/prazeres/index.js
@@ -1,5 +1,3 @@
-import { Link } from 'react-router-dom';
-
 import Layout from '../../Layout';
 import {
   ApresentandoOProgramaUm,
@@ -24,76 +22,137 @@ import {
   Equipe
 } from '../../../assets';
 
-import * as S from './styles';
+import * as S from '../globalPhotoStyle';
   
 function CommunitiesGalleries() {
   const listCommunities = [
     {
       id: 0,
+      type: '',
       name: 'Apresentando o programa',
       photos: [
-        ApresentandoOProgramaUm,
-        ApresentandoOProgramaDois,
-        ApresentandoOProgramaTres,
-        ApresentandoOProgramaSete,
-        ApresentandoOProgramaCinco,
-        ApresentandoOProgramaSeis,
-        ApresentandoOProgramaQuatro,
-        ApresentandoOProgramaOito,
-        ApresentandoOProgramaNove,
+        {
+          img: ApresentandoOProgramaUm,
+          type: '',
+        },
+        {
+          img: ApresentandoOProgramaDois,
+          type: '',
+        },
+        {
+          img: ApresentandoOProgramaTres,
+          type: '',
+        },
+        {
+          img: ApresentandoOProgramaSete,
+          type: '',
+        },
+        {
+          img: ApresentandoOProgramaCinco,
+          type: '',
+        },
+        {
+          img: ApresentandoOProgramaSeis,
+          type: '',
+        },
+        {
+          img: ApresentandoOProgramaQuatro,
+          type: 'standing',
+        },
+        {
+          img: ApresentandoOProgramaOito,
+          type: 'standing',
+        },
+        {
+          img: ApresentandoOProgramaNove,
+          type: 'standing',
+        }
       ]
     },
     {
       id: 1,
       name: 'Marcos Bandeira, Dançarino e Coreógrafo',
       photos: [
-        MarcosBandeiraDancarinoeCoreografoDois,
-        MarcosBandeiraDancarinoeCoreografoQuatro,
-        MarcosBandeiraDancarinoeCoreografoUm,
-        MarcosBandeiraDancarinoeCoreografoTres,
-        MarcosBandeiraDancarinoeCoreografoCinco
+        {
+          img: MarcosBandeiraDancarinoeCoreografoDois,
+          type: '',
+        },
+        {
+          img: MarcosBandeiraDancarinoeCoreografoQuatro,
+          type: '',
+        },
+        {
+          img: MarcosBandeiraDancarinoeCoreografoUm,
+          type: 'standing',
+        },
+        {
+          img: MarcosBandeiraDancarinoeCoreografoTres,
+          type: 'standing',
+        },
+        {
+          img: MarcosBandeiraDancarinoeCoreografoCinco,
+          type: 'standing',
+        }
       ]
     },
     {
       id: 2,
       name: 'Charles Siqueira - Coordenador do Programa',
       photos: [
-        CharlesSiqueira,
+        {
+          img: CharlesSiqueira,
+          type: '',
+        }
       ]
     },
     {
       id: 3,
       name: 'Cris dos Prazeres - Instituto Precisa Ser',
       photos: [
-        CrisdosPrazeres,
+        {
+          img: CrisdosPrazeres,
+          type: 'standing',
+        }
       ]
     },
     {
       id: 4,
       name: 'Genival André - Líderança dos Prazeres',
       photos: [
-        GenivalAndre,
+        {
+          img: GenivalAndre,
+          type: '',
+        }
       ]
     },
     {
       id: 5,
       name: 'Isaura Bredariol - Parque Nacional da Tijuca',
       photos: [
-        IsauraBredariol,
+        {
+          img: IsauraBredariol,
+          type: '',
+        }
       ]
     },
     {
       id: 6,
       name: 'DJ 2B',
       photos: [
-        DJ2B,
+        {
+          img: DJ2B,
+          type: 'standing',
+        }
       ]
     },
     {
       id: 7,
       name: 'Equipe',
       photos: [
-        Equipe,
+        {
+          img: Equipe,
+          type: '',
+        }
       ]
     },
   ];
@@ -101,21 +160,24 @@ function CommunitiesGalleries() {
   return (
     <Layout>
       <S.BackText>
-        Lançamento do projeto no Morro dos Prazeres.
+        Lançamento do Projeto no Morro dos Prazeres.
       </S.BackText>
      
-      {listCommunities.map(i => (
-        <>
-          <S.BackText>{i.name}</S.BackText>
-          {i?.photos?.map(img => (
-            <S.Img src={img} alt='' align="left"/>
-          ))}
-        </>
-      ))}
+      <S.ContainerPhoto>
+        {listCommunities.map(i => (
+          <>
+            <S.BackText subtitle>{i.name}</S.BackText>
+            <S.ContentPhoto>
+              {i?.photos?.map(imagem => (
+                <S.Img src={imagem.img} alt='' align="left" type={imagem.type} />
+                ))}
+            </S.ContentPhoto>
+          </>
+        ))} 
+      </S.ContainerPhoto>
     </Layout>
   );
 }
   
 export default CommunitiesGalleries;
   
-
