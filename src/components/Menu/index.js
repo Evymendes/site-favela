e81ca6985a8ceb831b4cdcfb6dd5e0e6104d5a24
Cicero <ciceropzr@gmail.com>
2024-12-15 14:41:34 -0300

    Alteração do arquivo do edital, correção no menu

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,7 +1,5 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useState } from 'react';
 import Logo from '../../assets/logo.png';
-
 import * as S from './styles';
 
 function Menu() {
@@ -46,16 +44,14 @@ function Menu() {
     {
       id: 7,
       name: 'Videoaula',
-      anchor: '#Videoaula',
-      isDisabled: true,
+      anchor: 'https://www.youtube.com/@favelaparque',
+      // isDisabled: true,
+      externalLink: true,
     },
   ];
 
   const renderIconBurger = () => (
-    <S.StyledBurger
-      open={open}
-      onClick={() => setOpen(!open)}
-    >
+    <S.StyledBurger open={open} onClick={() => setOpen(!open)}>
       <div />
       <div />
       <div />
@@ -63,20 +59,57 @@ function Menu() {
   );
 
   return (
-    <S.Header id='topo'>
+    <S.Header id="topo">
       <S.Nav>
-        <a href='#'>
-          <S.Logo src={Logo} alt='Logo' />
+        <a href="#">
+          <S.Logo src={Logo} alt="Logo" />
         </a>
         {renderIconBurger()}
         <S.Ul open={open}>
-          {listMenu.map(item => (
+          {listMenu.map((item) => (
             <S.Li key={item.id} disabled={item.isDisabled}>
-              <a href={item.anchor} onClick={() => setOpen(false)}>
+              <a
+                href={item.anchor}
+                onClick={() => setOpen(false)}
+                target={item.externalLink ? '_blank' : '_self'}
+                rel={item.externalLink ? 'noopener noreferrer' : ''}
+              >
                 {item.name}
               </a>
             </S.Li>
           ))}
+          <S.ListContacts>
+            <li>
+              <a
+                rel="noopener noreferrer"
+                target="_blank"
+                href='https://pt-br.facebook.com/favelaparque/'
+              >
+                <i class='bx bxl-facebook-square'></i>
+
+              </a>
+            </li>
+            <li>
+              <a
+                rel="noopener noreferrer"
+                target="_blank"
+                href='https://instagram.com/favelaparque?igshid=YmMyMTA2M2Y='
+              >
+                <i class='bx bxl-instagram' ></i>
+
+              </a>
+            </li>
+            <li>
+              <a
+                rel="noopener noreferrer"
+                target="_blank"
+                href='https://api.whatsapp.com/send?phone=5521993660821'
+              >
+                <i class='bx bxl-whatsapp' ></i>
+
+              </a>
+            </li>
+          </S.ListContacts>
         </S.Ul>
       </S.Nav>
     </S.Header>
